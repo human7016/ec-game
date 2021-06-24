a09adaf63a968aded9d04ed05e5a07200314e8d8
human7016 <on.82.hoshi.naohiro@gmail.com>
2021-06-24 14:07:21 +0900

    change

--- src/components/Games.js
+++ src/components/Games.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import "firebase/firestore";
 import {db, storage} from "../base";
-import Gallery from "./Gallery"
+import Image from "./Image";
 
 
 export default function Games(){
@@ -11,10 +11,9 @@ export default function Games(){
         {/*try-catch構文:try(実行処理)catch(例外処理)*/}
         try {
             const data = db.collection('games');
-            let image;
             let response = await data.get()
-            //この書き方があっているかどうか
-            response.docs.get('photo');
+            //ここがおかしいっぽい
+            return response.docs;
             {/*response.docs.forEach((doc) =>
             console.dir(doc.get('category'))
             )*/}
@@ -25,9 +24,19 @@ export default function Games(){
     }
 
     return (
-      <div className="game-container">
-        {/* 子コンポーネントGalleryにURLの配列をプロパティとして渡す */}
-        <Gallery urls={getData}/>
+        <div className="columns is-vcentered is-multiline">
+        {/* mapメソッドでurls配列の値を一個ずつJSX式にしてImageに渡す */}
+        {/* map: 配列の値を一個ずつ処理する。変数urlはローカル変数 */}
+        {getData.map((url) => {
+          return (
+            <div key={url} className="column is-3">
+              {/*srcと言う名前のプロパティで画像のURLをImageコンポーネントに渡す 
+              親コンポーネントから子コンポーネントにプロパティを渡すときは
+              プロパティ名={値}またはプロパティ名="文字列"の形で書く*/} 
+              <Image src={url} />
+            </div>
+          );
+        })}
       </div>
     )
 }
