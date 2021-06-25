bf600cad0109afb2b25fb9bff18abc93e30b18ab
human7016 <on.82.hoshi.naohiro@gmail.com>
2021-06-25 18:33:23 +0900

    change

--- src/components/Games.js
+++ src/components/Games.js
@@ -10,10 +10,9 @@ export default function Games(){
     const getData = async () => {
         {/*try-catch構文:try(実行処理)catch(例外処理)*/}
         try {
-            const data = db.collection('games');
-            let response = await data.get()
-            //ここがおかしいっぽい
-            return response.docs;
+             db.collection('games').get().then((response) => {
+                 return response;
+             })
             {/*response.docs.forEach((doc) =>
             console.dir(doc.get('category'))
             )*/}
@@ -23,20 +22,25 @@ export default function Games(){
         }
     }
 
+    //console.log(getData());
     return (
         <div className="columns is-vcentered is-multiline">
         {/* mapメソッドでurls配列の値を一個ずつJSX式にしてImageに渡す */}
         {/* map: 配列の値を一個ずつ処理する。変数urlはローカル変数 */}
-        {getData.map((url) => {
-          return (
-            <div key={url} className="column is-3">
-              {/*srcと言う名前のプロパティで画像のURLをImageコンポーネントに渡す 
-              親コンポーネントから子コンポーネントにプロパティを渡すときは
-              プロパティ名={値}またはプロパティ名="文字列"の形で書く*/} 
-              <Image src={url} />
-            </div>
-          );
-        })}
-      </div>
+        {
+            getData().then((response) => {
+                console.log(response);
+                response.map((url) => {
+                    return (
+                        <div key={url} className="column is-3">
+                        {/*srcと言う名前のプロパティで画像のURLをImageコンポーネントに渡す 
+                        親コンポーネントから子コンポーネントにプロパティを渡すときは
+                        プロパティ名={値}またはプロパティ名="文字列"の形で書く*/} 
+                        <Image src={url} />
+                        </div>
+                    );
+                })
+            })
+}</div>
     )
 }
