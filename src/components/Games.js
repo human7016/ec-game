ac9bc4fe024f2a796d17fa19c8e058892bfcd49d
human7016 <on.82.hoshi.naohiro@gmail.com>
2021-07-05 19:00:40 +0900

    update

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,46 +1,68 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import React, {useState, useEffect} from "react";
 import "firebase/firestore";
 import {db, storage} from "../base";
 import Image from "./Image";
+import { SettingsSystemDaydreamSharp } from "@material-ui/icons";
+import {Button} from "@material-ui/core";
 
 
 export default function Games(){
-    {/*async:非同期関数宣言*/}
-    const getData = async () => {
-        {/*try-catch構文:try(実行処理)catch(例外処理)*/}
-        try {
-             db.collection('games').get().then((response) => {
-                 return response;
-             })
-            {/*response.docs.forEach((doc) =>
-            console.dir(doc.get('category'))
-            )*/}
-            //console.dir(storage.ref());
-        } catch (e) {
-            alert(e);
-        }
+    const [images, setImages] = useState([]);
+
+    async function docRef(){
+        let querySnapshot = await db.collection("games").get()
+        querySnapshot.forEach((doc) => {
+            images.push(
+                <div key={doc.data().photo} className="column is-3">
+                <Image src={doc.data().photo} />
+                </div>
+            )
+        });
+        return images;
     }
 
-    //console.log(getData());
-    return (
-        <div className="columns is-vcentered is-multiline">
-        {/* mapメソッドでurls配列の値を一個ずつJSX式にしてImageに渡す */}
-        {/* map: 配列の値を一個ずつ処理する。変数urlはローカル変数 */}
-        {
-            getData().then((response) => {
-                console.log(response);
-                response.map((url) => {
-                    return (
-                        <div key={url} className="column is-3">
-                        {/*srcと言う名前のプロパティで画像のURLをImageコンポーネントに渡す 
-                        親コンポーネントから子コンポーネントにプロパティを渡すときは
-                        プロパティ名={値}またはプロパティ名="文字列"の形で書く*/} 
-                        <Image src={url} />
-                        </div>
-                    );
-                })
-            })
-}</div>
+    function fetchImages() {
+        docRef().then((images) => {
+            setImages(images);
+        })
+    }
+
+    return(
+        <div>
+            <Button onClick={fetchImages}>GET DATA</Button>
+            {images}
+        </div>
     )
 }
+
+
+    // const getData = async () => {
+    //     {/*try-catch構文:try(実行処理)catch(例外処理)*/}
+    //     try {
+    //         db.collection('games').get().then((response) => {
+    //             return (
+    //                 <div className="columns is-vcentered is-multiline">
+    //                 {
+    //                     response.map((url) => {
+    //                         return (
+    //                             <div key={url} className="column is-3">
+    //                             <Image src={url} />
+    //                             </div>
+    //                         );
+    //                     })
+    //                 }
+    //                 </div>
+    //             )
+    //         })
+    //     } catch (e) {
+    //         alert(e);
+    //     }
+    // }
+    
+    // return (
+    //     <div>
+    //         <button onClick={fetchGames}>
+    //             click me 
+    //         </button>
+    //     </div>
+    // );
